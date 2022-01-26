983b6e9710d390b832682b3b9b12b6bca8aebbb5
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-01-26 21:07:01 +0700

    commit

---
+++ lib/route.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:sufismart/ui/all_news.dart';
+import 'package:sufismart/ui/home_view.dart';
+
+String initialRouteName = RouteName.splashScreen;
+
+class RouteName {
+  static const String splashScreen = "splashScreen";
+  static const String home = "home";
+  static const String allNews = "allNews";
+}
+
+Map<String, WidgetBuilder> route = {
+  RouteName.home: (BuildContext context) => HomeView(
+        gotoShowAll: () {
+          Navigator.pushNamed(context, RouteName.allNews);
+        },
+        gotoBranch: () {},
+        gotoCredit: () {},
+        gotoDetailNews: (e) {},
+        gotoForgotPassword: () {},
+        gotoInstallment: () {},
+        gotoPayment: () {},
+        gotoProduct: () {},
+        gotoPromo: () {},
+        gotoSignup: () {},
+      ),
+  RouteName.allNews: (BuildContext context) => AllNews(
+        gotoDetailNews: (e) {},
+      ),
+};
