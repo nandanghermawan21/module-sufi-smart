3d8ce80ccd8b088580b65e9e2dd0783d2348ae99
renoldiboz <renoldiboz@gmail.com>
2022-01-29 18:45:02 +0700

    change

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,19 +1,35 @@
 import UIKit
 import Flutter
 
+enum ChannelName {
+  static let battery = "samples.flutter.dev/battery"
+  static let charging = "samples.flutter.io/charging"
+}
+
+enum BatteryState {
+  static let charging = "charging"
+  static let discharging = "discharging"
+}
+
+enum MyFlutterErrorCode {
+  static let unavailable = "UNAVAILABLE"
+}
+
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterStreamHandler {
+  private var eventSink: FlutterEventSink?
+
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-
-     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/battery",
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    guard let controller = window?.rootViewController as? FlutterViewController else {
+      fatalError("rootViewController is not type FlutterViewController")
+    }
+    let batteryChannel = FlutterMethodChannel(name: ChannelName.battery,
                                               binaryMessenger: controller.binaryMessenger)
-     batteryChannel.setMethodCallHandler({
-    [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-      // Note: this method is invoked on the UI thread.
+    batteryChannel.setMethodCallHandler({
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
       guard call.method == "getBatteryLevel" else {
         result(FlutterMethodNotImplemented)
         return
@@ -21,12 +37,14 @@ import Flutter
       self?.receiveBatteryLevel(result: result)
     })
 
-    GeneratedPluginRegistrant.register(with: self)
+    let chargingChannel = FlutterEventChannel(name: ChannelName.charging,
+                                              binaryMessenger: controller.binaryMessenger)
+    chargingChannel.setStreamHandler(self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   private func receiveBatteryLevel(result: FlutterResult) {
-  let device = UIDevice.current
+    let device = UIDevice.current
   device.isBatteryMonitoringEnabled = true
   if device.batteryState == UIDevice.BatteryState.unknown {
     result(FlutterError(code: "UNAVAILABLE",
@@ -35,5 +53,48 @@ import Flutter
   } else {
     result(Int(device.batteryLevel * 100))
   }
-}
+     
+  }
+
+  public func onListen(withArguments arguments: Any?,
+                       eventSink: @escaping FlutterEventSink) -> FlutterError? {
+    self.eventSink = eventSink
+    UIDevice.current.isBatteryMonitoringEnabled = true
+    sendBatteryStateEvent()
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(AppDelegate.onBatteryStateDidChange),
+      name: UIDevice.batteryStateDidChangeNotification,
+      object: nil)
+    return nil
+  }
+
+  @objc private func onBatteryStateDidChange(notification: NSNotification) {
+    sendBatteryStateEvent()
+  }
+
+  private func sendBatteryStateEvent() {
+    guard let eventSink = eventSink else {
+      return
+    }
+
+    switch UIDevice.current.batteryState {
+    case .full:
+      eventSink(BatteryState.charging)
+    case .charging:
+      eventSink(BatteryState.charging)
+    case .unplugged:
+      eventSink(BatteryState.discharging)
+    default:
+      eventSink(FlutterError(code: MyFlutterErrorCode.unavailable,
+                             message: "Charging status unavailable",
+                             details: nil))
+    }
+  }
+
+  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    NotificationCenter.default.removeObserver(self)
+    eventSink = nil
+    return nil
+  }
 }
