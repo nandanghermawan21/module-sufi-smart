973abb2adbd567a7d850d46d3cce9d97084f5fa4
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-03 17:25:05 +0700

    commit

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:sufismart/model/news_model.dart';
 import 'package:sufismart/recource/color_default.dart';
 import 'package:sufismart/recource/string_id_id.dart';
+import 'package:sufismart/route.dart';
+import 'package:sufismart/util/mode_util.dart';
 import 'package:sufismart/util/one_signal_messaging.dart';
 import 'package:sufismart/util/system.dart';
 
@@ -14,5 +18,39 @@ void setting() {
   //setting oneSignal notification
   System.data.oneSignalMessaging = OneSignalMessaging(
     appId: "5950883a-0066-4be7-ac84-3d240982ffaf",
+    notificationHandler: (notification) {
+      ModeUtil.debugPrint("notification handler fire");
+    },
+    notificationOpenedHandler: (notification) {
+      ModeUtil.debugPrint("notification opened fire");
+    },
+    notificationClickedHandler: (notification) {
+      ModeUtil.debugPrint("notification handler fire");
+    },
   );
+  System.data.deepLinkingHandler = (uri) {
+    ModeUtil.debugPrint(uri?.path ?? "");
+    if (ModalRoute.of(System.data.context)?.settings.name == initialRouteName) {
+      return;
+    }
+    switch (uri?.path) {
+      case "/allNews":
+        Navigator.of(System.data.context).pushNamed(RouteName.allNews);
+        break;
+      case "/news":
+        NewsModel _news = NewsModel.dummy()
+            .where((e) =>
+                e.newsid == int.parse((uri?.queryParameters["id"] as String)))
+            .first;
+        Navigator.of(System.data.context).pushNamed(
+          RouteName.detailNews,
+          arguments: {
+            ParamName.newsModel: _news,
+          },
+        );
+        break;
+      default:
+        return;
+    }
+  };
 }
