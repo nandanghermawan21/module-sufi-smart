89d700ab6f203cf019556a7d3a6ab379c05efd3d
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-02-04 20:38:30 +0700

    commit

--- lib/view_model/credit_simulation_view_model.dart
+++ lib/view_model/credit_simulation_view_model.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sufismart/model/loan_type_model.dart';
 import 'package:sufismart/model/area_model.dart';
+import 'package:sufismart/model/insurance_type_model.dart';
+import 'package:intl/intl.dart';
+import 'package:sufismart/util/system.dart';
 
 class CreditSimulationViewModel extends ChangeNotifier {
   LoanTypeModel? _loanType;
@@ -20,6 +23,25 @@ class CreditSimulationViewModel extends ChangeNotifier {
   }
 
   List<AreaModel> areas = AreaModel.getAllArea();
+
+  final List<InsuraceTypeModel> _insuranceType = [];
+  List<InsuraceTypeModel> get insuranceType => _insuranceType;
+  void addInsuranceType(InsuraceTypeModel insuraceTypeModel) {
+    _insuranceType.add(insuraceTypeModel);
+    commit();
+  }
+
+  void removeInsuranceType(InsuraceTypeModel insuraceTypeModel) {
+    _insuranceType.remove(insuraceTypeModel);
+    commit();
+  }
+
+  bool insuranceTypeIsCheked(InsuraceTypeModel insuraceTypeModel) {
+    return _insuranceType.contains(insuraceTypeModel);
+  }
+
+  List<InsuraceTypeModel> insuranceTypes =
+      InsuraceTypeModel.getAllInsuranceType();
 
   void commit() {
     notifyListeners();
