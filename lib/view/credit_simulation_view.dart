31db86bcaf15f087b0d8872c4b692e6a331ebc9e
Agie003Annan <agie03annan@gmail.com>
2022-02-05 16:58:56 +0700

    commit agie sesi 3

--- lib/view/credit_simulation_view.dart
+++ lib/view/credit_simulation_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sufismart/component/basic_component.dart';
 import 'package:sufismart/view_model/credit_simulation_view_model.dart';
+import 'package:sufismart/util/system.dart';
+import 'package:sufismart/model/loan_type_model.dart';
+import 'package:sufismart/model/area_type_model.dart';
 
 class CreditSimulationView extends StatefulWidget {
   const CreditSimulationView({
@@ -28,12 +32,357 @@ class _CreditSimulationViewState extends State<CreditSimulationView> {
           child: Consumer<CreditSimulationViewModel>(
             builder: (c, d, w) {
               return Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(20),
-              );
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        System.data.strings!.creditSimulation
+                            .replaceAll("\n", " "),
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: System.data.color!.primaryColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        System.data.strings!
+                            .pleaseDoACreditSimulationUsingTheFormBelow
+                            .replaceAll("\n", " "),
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: System.data.color!.primaryColor,
+                            fontWeight: FontWeight.normal,
+                            wordSpacing: 2),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      loanType(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      area(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      price(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      insuranceType(),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Container(
+                            child: dpAmount(),
+                          )),
+                          const SizedBox(
+                            width: 30,
+                          ),
+                          Container(
+                            width: 100,
+                            color: Colors.transparent,
+                            child: dpPercentage(),
+                          )
+                        ],
+                      )
+                    ],
+                  ));
             },
           ),
         ),
+      ),
+    );
+  }
+
+  Widget loanType() {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            System.data.strings!.loanType,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(creditSimulationViewModel.loanTypes.length,
+                (index) {
+              return Expanded(
+                child: Row(
+                  children: [
+                    Radio<LoanTypeModel>(
+                      value: creditSimulationViewModel.loanTypes[index],
+                      onChanged: (val) {
+                        creditSimulationViewModel.loanType =
+                            creditSimulationViewModel.loanTypes[index];
+                      },
+                      activeColor: System.data.color!.primaryColor,
+                      groupValue: creditSimulationViewModel.loanType,
+                    ),
+                    Expanded(
+                      child: Container(
+                          width: double.infinity,
+                          height: 15,
+                          color: Colors.transparent,
+                          child: FittedBox(
+                            child: Text(creditSimulationViewModel
+                                    .loanTypes[index].name ??
+                                ""),
+                          )),
+                    ),
+                  ],
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget area() {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            System.data.strings!.area,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          DropdownButton<AreaModel>(
+            underline: Container(
+              height: 1,
+              color: System.data.color!.primaryColor,
+            ),
+            isExpanded: true,
+            hint: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  System.data.strings!.chooseArea,
+                ),
+              ],
+            ),
+            value: creditSimulationViewModel.area,
+            items:
+                List.generate(creditSimulationViewModel.areas.length, (index) {
+              return DropdownMenuItem<AreaModel>(
+                  value: creditSimulationViewModel.areas[index],
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(creditSimulationViewModel.areas[index].name ?? ""),
+                    ],
+                  ));
+            }),
+            onChanged: (area) {
+              creditSimulationViewModel.area = area;
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget insuranceType() {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            System.data.strings!.insuranceType,
+          ),
+          const SizedBox(
+            height: 0,
+          ),
+          Row(
+            children: List.generate(
+                creditSimulationViewModel.insuranceTypes.length, (index) {
+              return Expanded(
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: creditSimulationViewModel.insuranceTypeIsCheked(
+                          creditSimulationViewModel.insuranceTypes[index]),
+                      onChanged: (val) {
+                        if (val == true) {
+                          creditSimulationViewModel.addInsuranceType(
+                              creditSimulationViewModel.insuranceTypes[index]);
+                        } else {
+                          creditSimulationViewModel.removeInsuranceType(
+                              creditSimulationViewModel.insuranceTypes[index]);
+                        }
+                      },
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 15,
+                        alignment: Alignment.centerLeft,
+                        color: Colors.transparent,
+                        child: FittedBox(
+                          child: Text(creditSimulationViewModel
+                                  .insuranceTypes[index].name ??
+                              ""),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget price() {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            System.data.strings!.price,
+          ),
+          const SizedBox(
+            height: 0,
+          ),
+          TextField(
+            controller: creditSimulationViewModel.priceController,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp("[0-9]")),
+              TextInputFormatter.withFunction(
+                (oldValue, newValue) =>
+                    creditSimulationViewModel.priceFormater(oldValue, newValue),
+              ),
+            ],
+            onChanged: (val) {
+              creditSimulationViewModel.price =
+                  creditSimulationViewModel.parseDoubleFromString(val);
+            },
+            decoration: InputDecoration(
+              hintText: "Rp.",
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: System.data.color!.primaryColor, width: 1.0),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: System.data.color!.primaryColor, width: 1.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget dpAmount() {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            System.data.strings!.dpAmount,
+          ),
+          const SizedBox(
+            height: 0,
+          ),
+          TextField(
+            controller: creditSimulationViewModel.dpAmountController,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp("[0-9]")),
+              TextInputFormatter.withFunction((oldValue, newValue) =>
+                  creditSimulationViewModel.downPaymentAmountFormater(
+                      oldValue, newValue))
+            ],
+            onChanged: (val) {
+              creditSimulationViewModel.dpAmount =
+                  creditSimulationViewModel.parseDoubleFromString(val);
+            },
+            decoration: InputDecoration(
+              hintText: "Rp.",
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: System.data.color!.primaryColor, width: 1.0),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: System.data.color!.primaryColor, width: 1.0),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget dpPercentage() {
+    return Container(
+      width: double.infinity,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            System.data.strings!.min10percent,
+          ),
+          const SizedBox(
+            height: 0,
+          ),
+          TextField(
+            controller:
+                creditSimulationViewModel.downPaymentPercentageController,
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp("[0-9]")),
+              TextInputFormatter.withFunction(
+                (oldValue, newValue) => creditSimulationViewModel
+                    .downPaymentPercentageFormater(oldValue, newValue),
+              ),
+            ],
+            onChanged: (val) {
+              creditSimulationViewModel.dpPercent =
+                  creditSimulationViewModel.parseDoubleFromString(val);
+            },
+            decoration: InputDecoration(
+              hintText: "DP %",
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: System.data.color!.primaryColor, width: 1.0),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                    color: System.data.color!.primaryColor, width: 1.0),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/view_model/credit_simulation_view_model.dart
+++ lib/view_model/credit_simulation_view_model.dart
@@ -1,6 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:sufismart/model/loan_type_model.dart';
+import 'package:sufismart/model/area_type_model.dart';
+import 'package:sufismart/model/insurance_type_model.dart';
+
+import '../util/system.dart';
 
 class CreditSimulationViewModel extends ChangeNotifier {
+  LoanTypeModel? _loanType;
+  LoanTypeModel? get loanType => _loanType;
+  set loanType(LoanTypeModel? type) {
+    _loanType = type;
+    commit();
+  }
+
+  List<LoanTypeModel> loanTypes = LoanTypeModel.getForCreditSimulation();
+
+  AreaModel? _area;
+  AreaModel? get area => _area;
+  set area(AreaModel? area) {
+    _area = area;
+    commit();
+  }
+
+  List<AreaModel> areas = AreaModel.getAllArea();
+
+  final List<InsuraceTypeModel> _insuranceType = [];
+  List<InsuraceTypeModel> get insuranceType => _insuranceType;
+  void addInsuranceType(InsuraceTypeModel insuraceTypeModel) {
+    _insuranceType.add(insuraceTypeModel);
+    commit();
+  }
+
+  void removeInsuranceType(InsuraceTypeModel insuraceTypeModel) {
+    _insuranceType.remove(insuraceTypeModel);
+    commit();
+  }
+
+  bool insuranceTypeIsCheked(InsuraceTypeModel insuraceTypeModel) {
+    return _insuranceType.contains(insuraceTypeModel);
+  }
+
+  List<InsuraceTypeModel> insuranceTypes =
+      InsuraceTypeModel.getAllInsuranceType();
+
+  TextEditingController priceController = TextEditingController();
+  double? _price = 0;
+  double? get price => _price;
+  set price(double? price) {
+    _price = price;
+    calcPercentage();
+    commit();
+  }
+
+  TextEditingController dpAmountController = TextEditingController();
+  double? _dpAmount = 0;
+  double? get dpAmount => _dpAmount;
+  set dpAmount(double? amount) {
+    _dpAmount = amount;
+    calcPercentage();
+    commit();
+  }
+
+  TextEditingController downPaymentPercentageController =
+      TextEditingController();
+  double? _dpPercent = 0;
+  double? get dpPercent => _dpPercent;
+  set dpPercent(double? dpPercent) {
+    _dpPercent = dpPercent;
+    _dpAmount = (_price ?? 8) * (_dpPercent ?? 0) / 100;
+    dpAmountController.text = (_dpPercent ?? 0) == 0
+        ? ""
+        : "Rp. " +
+            NumberFormat("#,###", System.data.strings!.locale)
+                .format(_dpAmount);
+    commit();
+  }
+
+  TextEditingValue priceFormater(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    String _val =
+        newValue.text.replaceAllMapped(RegExp("[^0-9]"), (match) => "");
+    String _num = _val.isNotEmpty
+        ? "Rp. " +
+            NumberFormat("#,###", System.data.strings!.locale)
+                .format(double.parse(_val))
+        : "";
+
+    if ((parseDoubleFromString(_num) ?? 0) < (_dpAmount ?? 0)) {
+      dpAmount = null;
+      dpAmountController.clear();
+    }
+    return TextEditingValue(
+      composing: newValue.composing,
+      selection: TextSelection.fromPosition(TextPosition(offset: _num.length)),
+      text: _num,
+    );
+  }
+
+  TextEditingValue downPaymentAmountFormater(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    String _val =
+        newValue.text.replaceAllMapped(RegExp("[^0-9]"), (match) => "");
+    String _num = _val.isNotEmpty
+        ? "Rp. " +
+            NumberFormat("#,###", System.data.strings!.locale)
+                .format(double.parse(_val))
+        : "";
+
+    if ((parseDoubleFromString(_num) ?? 0) > (_price ?? 0)) {
+      return TextEditingValue(
+        composing: newValue.composing,
+        selection: TextSelection.fromPosition(
+            TextPosition(offset: oldValue.text.length)),
+        text: oldValue.text,
+      );
+    } else {
+      return TextEditingValue(
+        composing: newValue.composing,
+        selection:
+            TextSelection.fromPosition(TextPosition(offset: _num.length)),
+        text: _num,
+      );
+    }
+  }
+
+  TextEditingValue downPaymentPercentageFormater(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    String _val =
+        newValue.text.replaceAllMapped(RegExp("[^0-9]"), (match) => "");
+    String _num = (_val.isNotEmpty && (_price ?? 0) != 0)
+        ? double.parse(_val) > 100
+            ? oldValue.text.replaceFirst(" %", "")
+            : _val
+        : "";
+    return TextEditingValue(
+      composing: newValue.composing,
+      selection: TextSelection.fromPosition(TextPosition(offset: _num.length)),
+      text: (_num.isNotEmpty && (_price ?? 0) != 0) ? _num + " %" : "",
+    );
+  }
+
+  void calcPercentage() {
+    _dpPercent = ((price ?? 0) == 0) || ((_dpAmount ?? 0) == 0)
+        ? null
+        : (_dpAmount! / _price!) * 100;
+    downPaymentPercentageController.text =
+        _dpPercent == null ? "" : _dpPercent!.toStringAsFixed(2) + " %";
+  }
+
+  double? parseDoubleFromString(String priceString) {
+    return priceString.isNotEmpty
+        ? double.parse(
+            priceString.replaceAllMapped(RegExp("[^0-9]"), (match) => ""))
+        : null;
+  }
+
+  void calculate() {}
+
   void commit() {
     notifyListeners();
   }
