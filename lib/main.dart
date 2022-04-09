093f920a4d902bcf22587650b48b1adf4e9e4fe3
nandanghermawan <nandanghermawan21@GMAIL.COM>
2022-04-09 13:05:26 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,60 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_background_service_ios/flutter_background_service_ios.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:sufismart/component/circular_loader_component.dart';
 import 'package:sufismart/setting.dart';
 import 'package:sufismart/util/data.dart';
+import 'package:sufismart/util/enum.dart';
 import 'package:sufismart/util/mode_util.dart';
 import 'package:sufismart/util/system.dart';
 import 'package:sufismart/route.dart';
 import 'package:uni_links/uni_links.dart';
 import 'route.dart';
+import 'service.dart' as service;
 
 Data data = Data();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setting();
   data.initialize().then((val) async {
-    runApp(const MyApp());
+    initializeService().then((value) {
+      runApp(const MyApp());
+    });
+  });
+}
+
+Future<void> initializeService() async {
+  await data.service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStartService,
+      autoStart: true,
+      isForegroundMode: false,
+    ),
+    iosConfiguration: IosConfiguration(
+      autoStart: true,
+      onForeground: onStartService,
+      onBackground: onStartService,
+    ),
+  );
+}
+
+void onStartService() {
+  debugPrint("start services default");
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isIOS) FlutterBackgroundServiceIOS.registerWith();
+  if (Platform.isAndroid) FlutterBackgroundServiceAndroid.registerWith();
+  service.onServiceStarted();
+
+  data.service.onDataReceived.listen((event) {
+    service.onEvent(event);
   });
 }
 
@@ -68,7 +103,7 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> with WidgetsBindingObserver {
   bool _initialUriIsHandled = false;
 
   @override
@@ -80,27 +115,41 @@ class MyAppState extends State<MyApp> {
     getPermission().then((value) {
       initOnesignal();
     });
+    WidgetsBinding.instance?.addObserver(this);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ChangeNotifierProvider.value(
-        value: System.data,
-        child: Consumer<Data>(
-          builder: (c, d, w) {
-            return MaterialApp(
-              home: Scaffold(
-                resizeToAvoidBottomInset: false,
-                backgroundColor: Colors.transparent,
-                body: CircularLoaderComponent(
-                  controller: data.loadingController,
-                  child: home(),
-                ),
-              ),
-            );
-          },
-        ),
+      home: Stack(
+        children: [
+          StreamBuilder<Map<String, dynamic>?>(
+            stream: data.service.onDataReceived,
+            builder: (c, d) {
+              if (d.hasData && d.data != null) {
+                data.service.sendData(d.data!);
+              }
+              return const SizedBox();
+            },
+          ),
+          ChangeNotifierProvider.value(
+            value: System.data,
+            child: Consumer<Data>(
+              builder: (c, d, w) {
+                return MaterialApp(
+                  home: Scaffold(
+                    resizeToAvoidBottomInset: false,
+                    backgroundColor: Colors.transparent,
+                    body: CircularLoaderComponent(
+                      controller: data.loadingController,
+                      child: home(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -158,6 +207,33 @@ class MyAppState extends State<MyApp> {
           if (!mounted) return;
         },
       );
+    }
+  }
+
+  @override
+  void dispose() {
+    ModeUtil.debugPrint("APP Disposed");
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    ModeUtil.debugPrint("APP LifeCycle State");
+    System.data.service.sendData({
+      ServiceKey.action: ServiceValueAction.sendToForeground,
+    });
+    switch (state) {
+      case AppLifecycleState.resumed:
+        ModeUtil.debugPrint("APP Resume");
+        break;
+      case AppLifecycleState.inactive:
+        ModeUtil.debugPrint("APP Closed");
+        break;
+      case AppLifecycleState.paused:
+        break;
+      case AppLifecycleState.detached:
+        break;
     }
   }
 }
