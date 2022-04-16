49209f0165c8dff20b4273e72b4953509b5334f8
tori97 <toriy1997@gmail.com>
2022-04-16 17:28:08 +0700

    ok

--- lib/view_model/chat_view_model.dart
+++ lib/view_model/chat_view_model.dart
@@ -1,7 +1,10 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:sufismart/model/chat_model.dart';
 import 'package:sufismart/model/customer_model.dart';
 import 'package:sufismart/component/cilcular_loader_component.dart';
+import 'package:sufismart/model/notification_model.dart';
 import 'package:sufismart/util/error_handling_util.dart';
 import 'package:sufismart/util/mode_util.dart';
 import 'package:sufismart/util/system.dart';
@@ -51,6 +54,26 @@ class ChatViewModel extends ChangeNotifier {
       messageController.text = "";
       chats.add(chatModel);
       commit();
+
+      NotificationModel(
+      appId: "5950883a-0066-4be7-ac84-3d240982ffaf",
+        $apiKey: "63773c22-a638-49a1-b538-440bdd3b7975",
+      ).sendBasicNotif(
+        title: System.data.global.customerModel?.fullName??"", 
+        message: chatModel.message??"", 
+        receiver: reciver?.deviceid!=null ?[reciver?.deviceid??""]:[], 
+        appUrl: ""
+        ).then((value) {
+          chatModel.notificationId=value;
+          chatModel.status=1;
+          commit();
+
+        }).catchError((onError){
+          loadingController.stopLoading(
+            isError: true,
+            message: onError
+          );
+        });
     }).catchError(
       (onError) {
         loadingController.stopLoading(
