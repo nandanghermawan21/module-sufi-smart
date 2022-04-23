b856d98a1ca100713cb44e3cb82170ac7ab1e27e
tori97 <toriy1997@gmail.com>
2022-04-23 10:32:09 +0700

    ok

--- lib/util/global.dart
+++ lib/util/global.dart
@@ -7,4 +7,8 @@ class Global {
   String ocrKey =
       "706a9adfff2ee547042c40155922144086bb22bf6c558b1ca95fd42293dc0348";
   CustomerModel? customerModel;
+
+  get notifAppId => null;
+
+  get notifAppKey => null;
 }
