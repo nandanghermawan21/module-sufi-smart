b9142748c226cb932080bff96bcc2c30c8d75bc9
renoldiboz <renoldiboz@gmail.com>
2022-05-17 19:18:25 +0700

    change

--- android/app/src/main/kotlin/com/example/sufismart/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sufismart/MainActivity.kt
@@ -1,72 +1,31 @@
 package com.example.sufismart
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.os.Build
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-
+import android.app.KeyguardManager
 import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import android.content.IntentFilter
-import android.os.BatteryManager
-import android.os.Build.VERSION_CODES
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 
+class MainActivity : FlutterFragmentActivity() {
 
-class MainActivity: FlutterFragmentActivity() {
-    private val CHANNEL = "samples.flutter.dev/battery"
-
-    lateinit var notificationManager: NotificationManager
-    lateinit var notificationChannel: NotificationChannel
-    lateinit var builder: Notification.Builder
-    private val channelId = "i.apps.notifications"
-    private val description = "Test notification"
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            // Note: this method is invoked on the main thread.
-            call, result ->
-            if (call.method == "getBatteryLevel") {
-                val batteryLevel = getBatteryLevel()
-
-                if (batteryLevel != -1) {
-                    result.success(batteryLevel)
-                } else {
-                    result.error("UNAVAILABLE", "Battery level not available.", null)
-                }
-            }
-            else if (call.method == "runService") {
-//                startService(Intent(applicationContext, BackNotife::class.java))
-//                BackNotife.startService(this, "Foreground Service is running...")
-
-            }
-            else if (call.method == "stopService") {
-//                startService(Intent(, BackNotife::class.java))
-                BackNotife.stopService(this,)
-            }
-            else {
-                result.notImplemented()
-            }
-        }
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        showWhenLockedAndTurnScreenOn()
+        super.onCreate(savedInstanceState, persistentState)
     }
 
-    private fun getBatteryLevel(): Int {
-        val batteryLevel: Int
-        if (Build.VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    private fun showWhenLockedAndTurnScreenOn() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
         } else {
-            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+            )
         }
-
-        return batteryLevel
     }
 }
