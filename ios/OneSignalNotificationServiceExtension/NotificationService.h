b00e93653cbb45cdc15814a26b13916496d7ab97
agie003annan <agie03annan@gmail.com>
2022-09-12 15:15:35 +0700

    commit agie 20220912 2 15.15

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by SFI on 08/09/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
