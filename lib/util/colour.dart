dba6205a147b7bced50e1b5916daac9f799ede3d
agie003annan <agie03annan@gmail.com>
2022-11-02 15:24:40 +0700

    commit_agie_20221102

--- lib/util/colour.dart
+++ lib/util/colour.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 abstract class Colour {
   Color get mainColor => const Color(0xFF0d306b);
-  Color get background => const Color(0XFFEEEEEE);0d306b
-  Color get primaryColor => const Color(0xff);
+  Color get background => const Color(0XFFEEEEEE);
+  Color get primaryColor => const Color(0xff0d306b);
   Color get greyColor => const Color(0xFF808080);
   Color get whiteColor => const Color(0xFFFFFFFF);
   Color get whiteSmoke => const Color(0xFFF2F2F2);
